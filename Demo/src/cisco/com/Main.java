425575aacebb9a23ca81bcec25b785e9d02077cc
sdushe <sdushe@SDUSHE-WS02.cisco.com>
2015-05-28 15:18:59 -0700

    First Commit

---
+++ Demo/src/cisco/com/Main.java
@@ -0,0 +1,18 @@
+package cisco.com;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Sample smp=new Sample();
+		smp.displayData();
+		displayPrintStatement();
+		
+    }
+	
+	public static void displayPrintStatement()
+	{
+		System.out.println("Test1");
+	}
+
+}
---
+++ Demo/src/cisco/com/Sample.java
@@ -0,0 +1,12 @@
+package cisco.com;
+
+public class Sample {
+	
+	int a=10;
+	
+	public void displayData()
+	{
+		System.out.println("Value of a :"+ a);
+	}
+	
+}
